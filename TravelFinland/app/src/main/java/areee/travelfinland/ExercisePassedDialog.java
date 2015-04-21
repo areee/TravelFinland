747f5893513dab72c5e277c5931a221d23aa74d3
Arttu Ylhävuori <areee@users.noreply.github.com>
2015-04-21 22:23:21 +0300

    added new features

--- TravelFinland/app/src/main/java/areee/travelfinland/ExercisePassedDialog.java
+++ TravelFinland/app/src/main/java/areee/travelfinland/ExercisePassedDialog.java
@@ -1,31 +1,52 @@
 package areee.travelfinland;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class ExercisePassedDialog extends DialogFragment {
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setStyle(DialogFragment.STYLE_NO_TITLE,R.style.MyDialog);
+//    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        Window window = getDialog().getWindow();
+//        window.setBackgroundDrawableResource(android.R.color.transparent);
+//    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.exercise_text)
-                .setPositiveButton(R.string.check_button, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                })
-                .setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.exercise_passed_dialog_layout, null));
 
-                    }
-                });
+//        builder.setMessage(R.string.right_answer_text);
+
+        builder.setPositiveButton(R.string.continue_button_text, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getActivity().finish();
+//                dialog.dismiss();
+//                ((Activity) context).finish();
+            }
+        });
 
         return builder.create(); //super.onCreateDialog(savedInstanceState)
     }
+
+//    public void finishActivity(){
+//
+//    }
 }
